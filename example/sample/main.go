b41e73c631a2d944c1e07dbcf59915681549a1f8
Lauris BH <lauris@nix.lv>
2021-11-21 20:34:19 +0200

    Update sample

--- example/sample/main.go
+++ example/sample/main.go
@@ -13,9 +13,9 @@ import (
 
 func main() {
 	size := 80
-	img := image.NewRGBA(image.Rect(0, 0, size*6, size*2))
+	img := image.NewRGBA(image.Rect(0, 0, size*10, size*4))
 
-	for i := 0; i < 12; i++ {
+	for i := 0; i < 40; i++ {
 		seed := time.Now().Format(time.RFC3339Nano)
 		buf, err := avatars.PNG(seed, avatars.RenderSize(size))
 		if err != nil {
@@ -25,10 +25,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		pos := image.Rect(i*size, 0, (i+1)*size, size)
-		if i >= 6 {
-			pos = image.Rect((i-6)*size, size, (i-5)*size, size*2)
-		}
+		pos := image.Rect((i-int(i/10)*10)*size, int(i/10)*size, ((i-int(i/10)*10)+1)*size, (int(i/10)+1)*size)
 		draw.Draw(img, pos, av, image.Point{}, draw.Over)
 	}
 
